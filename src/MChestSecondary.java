0aca2d98a643ce359da77d3211ee1d4f11e36e26
yuvi-atre <122226040+yuvi-atre@users.noreply.github.com>
2025-03-25 23:52:58 -0400

    Finished secondary class

---
+++ src/MChestSecondary.java
@@ -0,0 +1,95 @@
+import components.list.List;
+import components.list.List2;
+import components.map.Map;
+import components.map.Map.Pair;
+import components.map.Map2;
+
+/**
+ * Abstract class that implements the secondary methods of MChest.
+ */
+public abstract class MChestSecondary implements MChest {
+
+    // CHECKSTYLE: ALLOW THIS METHOD TO BE OVERRIDDEN
+    @Override
+    public Map<String, Integer> getItems() {
+        Map<String, Integer> itemsCopy = new Map2<>();
+        for (Pair<String, Integer> pair : this.getItems()) {
+            itemsCopy.add(pair.key(), pair.value());
+        }
+        return itemsCopy;
+    }
+
+    // CHECKSTYLE: ALLOW THIS METHOD TO BE OVERRIDDEN
+    @Override
+    public boolean canCraft(String... items) {
+        assert items != null
+                && items.length > 0 : "items must not be null or empty";
+        for (String item : items) {
+            if (!this.containsItem(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // CHECKSTYLE: ALLOW THIS METHOD TO BE OVERRIDDEN
+    @Override
+    public int totalItems() {
+        int total = 0;
+        for (Pair<String, Integer> pair : this.getItems()) {
+            total += pair.value();
+        }
+        return total;
+    }
+
+    // CHECKSTYLE: ALLOW THIS METHOD TO BE OVERRIDDEN
+    @Override
+    public List<String> getItemsByQuantity(int minQuantity) {
+        assert minQuantity >= 0 : "minQuantity must be non-negative";
+        List<String> result = new List2<>();
+        for (Pair<String, Integer> pair : this.getItems()) {
+            if (pair.value() >= minQuantity) {
+                result.addRightFront(pair.key());
+            }
+        }
+        return result;
+    }
+
+    // CHECKSTYLE: ALLOW THIS METHOD TO BE OVERRIDDEN
+    @Override
+    public void clear() {
+        while (this.getItems().size() > 0) {
+            Pair<String, Integer> pair = this.getItems().removeAny();
+            this.removeItem(pair.key(), pair.value());
+        }
+    }
+
+    // CHECKSTYLE: ALLOW THIS METHOD TO BE OVERRIDDEN
+    @Override
+    public String toString() {
+        String result = "MChest: ";
+        for (Pair<String, Integer> pair : this.getItems()) {
+            result += "[" + pair.key() + ", " + pair.value() + "] ";
+        }
+        return result.trim();
+    }
+
+    // CHECKSTYLE: ALLOW THIS METHOD TO BE OVERRIDDEN
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof MChest)) {
+            return false;
+        }
+        MChest other = (MChest) obj;
+        return this.getItems().equals(other.getItems());
+    }
+
+    // CHECKSTYLE: ALLOW THIS METHOD TO BE OVERRIDDEN
+    @Override
+    public int hashCode() {
+        return this.getItems().hashCode();
+    }
+}
